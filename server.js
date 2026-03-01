bb7ff0790aa75ddf0c66d8dd403f8caf246a3dff
SakshyamStha <sakshamstha6785@gmail.com>
2026-03-01 22:57:49 +0545

    implement CRUD operations for data and update test cases

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-let data = {
-    message: 'Hello data!',
-    timestamp: new Date()
-};
+let data = [ 'Ram', 'Shyam', 'Hari' ];
+
+// middleware
+app.use(express.json());  // for parsing application/json
 
 // website endpoints
 app.get('/', (req,res)=>{
     // res.sendStatus(200);
-    res.json({ message: 'Hello Home!',
-          requestMethod: req.method });
+    // res.json({ message: 'Hello Home!',
+    //       requestMethod: req.method });
+    res.send(`
+        <body>
+        <h1>Data</h1>
+            <p>
+                ${JSON.stringify(data)}
+            </p>
+        </body>
+        `);
 })
 app.get('/dashboard', (req,res)=>{
     // res.sendStatus(200);
@@ -20,11 +28,21 @@ app.get('/dashboard', (req,res)=>{
 })
 
 
+//  CRUD operations
+//  crud-method:  Create- Post. Read- Get, Update- Put. Delete- delete
+
 // api endpoints
 app.get('/api/data', (req,res) => {
     res.json({ data: data,
           requestMethod: req.method });
 })
+
+app.post('/api/data', (req,res) => {
+    const newEntry = req.body;  // a payload body from frontend
+    data.push(newEntry.name);
+    res.status(201);
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
