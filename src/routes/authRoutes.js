8961b137d5014548219b9ebb0ca0783c3d15ca5d
SakshyamStha <sakshamstha6785@gmail.com>
2026-03-04 18:50:50 +0545

    feat: implement authentication routes for user registration and login

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import db from '../db.js';
+
+const router = express.Router();
+
+router.post('/register', (req,res) => {
+    const {name, email, password} = req.body;
+    res.sendStatus(201);
+
+})
+router.post('/login', (req,res) => {
+
+})
+
+export default router;
